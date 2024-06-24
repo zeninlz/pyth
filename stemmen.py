09f881648cc75f2c0c1b6300aa1ccd5184d1e791
martino simic <131748598+zeninlz@users.noreply.github.com>
2024-06-24 20:51:34 +0200

    Update stemmen.py

--- stemmen.py
+++ stemmen.py
@@ -1,29 +1,29 @@
 
-stemmen_omer = 0
-stemmen_mehmet = 0
+stemmen_jef = 0
+stemmen_martino = 0
 
 print("Welkom bij de verkiezingen van Florijnia!")
-print("Je kunt stemmen op Omer of Mehmet.")
+print("Je kunt stemmen op jef of martino.")
 print("Type 'UITSLAG' om de stemmen te stoppen en de winnaar te zien.\n")
 
 stem = input("Op wie wil je stemmen? ")
 
 while stem != 'UITSLAG':
 
-    if stem.lower() == 'omer':
-        stemmen_omer += 1
-    elif stem.lower() == 'mehmet':
-        stemmen_mehmet += 1
+    if stem.lower() == 'jef':
+        stemmen_jef += 1
+    elif stem.lower() == 'martino':
+        stemmen_martino += 1
     else:
         print("Ongeldige keuze. Probeer opnieuw.")
 
     stem = input("Op wie wil je stemmen? ")
 
 # Bepalen van de winnaar op basis van het aantal stemmen
-if stemmen_omer > stemmen_mehmet:
+if stemmen_omer > martino:
     print("Omer heeft gewonnen!")
-elif stemmen_mehmet > stemmen_omer:
-    print("Mehmet heeft gewonnen!")
+elif stemmen_martino > stemmen_jef:
+    print("martino heeft gewonnen!")
 else:
     print("Het is een gelijkspel!")
 
